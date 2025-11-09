2a109937405684a3d8c4239728f9289d6f9feab6
Gabriel Rolon <rolon_gabriel@outlook.com>
2025-11-09 17:02:58 -0300

    Fix(CSP): Arreglar políticas de Helmet y exportación de modelo

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 'use strict';
-require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const expect = require('chai').expect;
+const cors = require('cors');
+const helmet = require('helmet'); 
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
-const helmet = require('helmet');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcc-testing.js');
+const runner = require('./test-runner');
 
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); 
+app.use(cors({ origin: '*' })); 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'"],
+    styleSrc: ["'self'"],
+  }
+}));
+
 app.use(helmet.frameguard({ action: 'deny' }));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
@@ -24,32 +33,33 @@ app.use(helmet.hsts({ maxAge: 7776000 }));
 app.disable('x-powered-by');
 
 app.route('/')
-  .get(function (req, res) {
+  .get(function(req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
 fccTestingRoutes(app);
 
-apiRoutes(app);  
-    
+apiRoutes(app);
+
 app.use(function(req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
 });
 
-const listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
-    setTimeout(function () {
+    setTimeout(function() {
       try {
         runner.run();
-      } catch(e) {
-        console.log('Tests are not valid:');
-        console.error(e);
+      } catch (e) {
+        let error = e;
+        console.log('Tests are not solid!');
+        console.log(error);
       }
-    }, 3500);
+    }, 1500);
   }
 });
 
